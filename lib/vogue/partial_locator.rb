3272765ae5112fc977970531b049b8eca0272baa
Sean St. Quentin <sean@iproperty.com.au>
2010-07-16 13:46:49 +1000

    Don't want to raise a special exception when vogue is enabled and the template can't be found, given how we're overriding things.

--- lib/vogue/partial_locator.rb
+++ lib/vogue/partial_locator.rb
@@ -10,7 +10,7 @@ module Vogue
     def _pick_partial_template_with_vogue(partial_path) #:nodoc:
       _pick_partial_template_without_vogue(partial_path)
     rescue ActionView::MissingTemplate
-      raise "Attempting to call a resource_controller_views partial from a controller which isn't vogue enabled." if !controller.respond_to?(:vogue_data)
+      raise if !controller.respond_to?(:vogue_data)
       raise if controller.nil? || controller.vogue_data[:root].blank?
       
       scaffold_paths = view_paths.class.new
